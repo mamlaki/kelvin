fddeb8ea572c0fea55d12946431ca43c997625b1
mamlaki <melekredwan@icloud.com>
2023-08-30 23:40:44 -0600

    Added some starting auth files.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import { ThemeProvider } from "@emotion/react"
+import theme from "./theme"
+import { SessionProvider } from "next-auth/react"
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <ThemeProvider
+      theme={ theme }
+      >
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SessionProvider>
+    
+  )
 }
