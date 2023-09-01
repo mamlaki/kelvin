5f2fc8d88c37ed121134fd29c7404cab16f40478
mamlaki <melekredwan@icloud.com>
2023-09-01 13:05:50 -0600

    Removed temp theme for auth testing purposes.

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,9 @@
-import { ThemeProvider, createTheme } from "@mui/material/styles"
-import themeOptions from "./theme"
-import { SessionProvider } from "next-auth/react"
+// import { ThemeProvider, createTheme } from "@mui/material/styles"
+// import themeOptions from "./theme"
+// import { SessionProvider } from "next-auth/react"
 
-const theme = createTheme(themeOptions)
+// const theme = createTheme(themeOptions)
 
-export default function App({ Component, pageProps: { session, ...pageProps } }) {
-  return (
-    <SessionProvider session={session}>
-      <ThemeProvider
-      theme={ theme }
-      >
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </SessionProvider>
-    
-  )
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />
 }
