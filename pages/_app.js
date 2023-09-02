8a8f0d6aa6b03fec7fd1e0c84a88dde47f8cdc96
mamlaki <melekredwan@icloud.com>
2023-09-02 00:26:52 -0600

    Cleaned up index.js and added Roboto and MUI Icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
-// import { ThemeProvider, createTheme } from "@mui/material/styles"
-// import themeOptions from "./theme"
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import { SessionProvider } from "next-auth/react"
 
-// const theme = createTheme(themeOptions)
 
 export default function App({ Component, pageProps }) {
     return (
