3070ae42a53a0e1d0456acde5b47addd5498f55a
mamlaki <melekredwan@icloud.com>
2023-09-01 19:36:49 -0600

    Added and tested the DiscordProvider.

--- components/Login.js
+++ components/Login.js
@@ -1,10 +1,12 @@
 import { useSession, signIn } from 'next-auth/react'
+import { red } from '@mui/material/colors'
 import Button from '@mui/material/Button'
 import Grid from '@mui/material/Grid'
 
 
 export default function Login() {
     const { data: session } = useSession()
+    const color = red[500]
 
     return (
         <>
@@ -26,6 +28,15 @@ export default function Login() {
                         onClick={() => signIn('github')}
                         >
                             Sign In with GitHub
+                        </Button>
+                    </Grid>
+                    <Grid item>
+                        <Button
+                        variant='contained'
+                        color='error'
+                        onClick={() => signIn('discord')}
+                        >
+                            Sign In with Discord
                         </Button>
                     </Grid>
                 </Grid>
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import GitHubProvider from 'next-auth/providers/github'
+import DiscordProvider from 'next-auth/providers/discord'
 // import EmailProvider from 'next-auth/providers/email'
 
 export default NextAuth({
@@ -12,6 +13,10 @@ export default NextAuth({
         GitHubProvider({
             clientId: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET
+        }),
+        DiscordProvider({
+            clientId: process.env.DISCORD_CLIENT_ID,
+            clientSecret: process.env.DISCORD_CLIENT_SECRET
         })
         // EmailProvider({
         //     server: {
