a259682f333f4945b2b82cce5537b11cc76152df
mamlaki <melekredwan@icloud.com>
2023-09-05 21:48:18 -0600

    Added some temp styling to the registration page.

--- pages/register.js
+++ pages/register.js
@@ -1,6 +1,15 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
 
+import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import EmailIcon from '@mui/icons-material/Email';
+import VpnKeyIcon from '@mui/icons-material/VpnKey';
+
 export default function Register() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
@@ -34,38 +43,71 @@ export default function Register() {
   }
 
   return (
-    <div>
+    <Container component='main' maxWidth='xs'>
       <h1>Register</h1>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor='username'>Username</label>
-          <input
-            type='text'
-            id='username'
-            value={ username }
-            onChange={ (event) => setUsername(event.target.value) }
-          />
-        </div>
-        <div>
-          <label htmlFor='password'>Password</label>
-          <input
-            type='password'
-            id='password'
-            value={ password }
-            onChange={ (event) => setPassword(event.target.value) }
-          />
-        </div>
-        <div>
-          <label htmlFor='email'>Email</label>
-          <input
-            type='email'
-            id='email'
-            value={ email }
-            onChange={ (event) => setEmail(event.target.value) }
-          />
-        </div>
-        <button type='submit'>Register Test</button>
+        <TextField 
+          fullWidth
+          margin='normal'
+          required
+          label='Username'
+          value={ username }
+          onChange={ (event) => setUsername(event.target.value) }
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position='start'>
+                <IconButton>
+                  <AccountCircle />
+                </IconButton>
+              </InputAdornment>
+            )
+          }}
+        />
+        <TextField 
+          fullWidth
+          margin='normal'
+          required
+          label='Email'
+          type='email'
+          value={ email }
+          onChange={ (event) => setEmail(event.target.value) }
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position='start'>
+                <IconButton>
+                  <EmailIcon />
+                </IconButton>
+              </InputAdornment>
+            )
+          }}
+        />
+        <TextField 
+          fullWidth
+          margin='normal'
+          required
+          label='Password'
+          type='password'
+          value={ password }
+          onChange={ (event) => setPassword(event.target.value) }
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position='start'>
+                <IconButton>
+                  <VpnKeyIcon />
+                </IconButton>
+              </InputAdornment>
+            )
+          }}
+        />
+        <Button
+          type='submit'
+          fullWidth
+          variant='contained'
+          color='primary'
+        >
+          Register
+        </Button>
       </form>
-    </div>
+    </Container>
   )
 }
