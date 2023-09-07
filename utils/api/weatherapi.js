b1b62bc7fde7d70d64ba36928222dcd147002da0
mamlaki <melekredwan@icloud.com>
2023-09-07 00:30:50 -0600

    Switched async fetch to promise.

--- utils/api/weatherapi.js
+++ utils/api/weatherapi.js
@@ -2,16 +2,16 @@ import { BASE_URL } from "./base";
 
 const API_KEY = '7cc6e0a83f9a403f080358c184ad3562'
 
-export const getWeatherData = async (cityName) => {
-  try {
-    const response = await fetch(`${BASE_URL}?q=${cityName}&appid=${API_KEY}`)
-    if (!response.ok) {
-      throw new Error('failed to fetch weather data')
-    }
-    const data = await response.json()
-    return data
-  } catch (error) {
-    console.error('Error fetching weather data: ', error)
-    throw error
-  }
+export const getWeatherData = (cityName) => {
+  return fetch(`${BASE_URL}?q=${cityName}&appid=${API_KEY}`)
+    .then(response => {
+      if(!response.ok) {
+        throw new Error('Failed to fetch weather data')
+      }
+      return response.json()
+    }).then(data => data)
+    .catch(error => {
+      console.error('Error fetching weather data: ', error)
+      throw error
+    })
 }
