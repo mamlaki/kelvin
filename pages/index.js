54d3545498a52307c02eddb585d33c12fca65c1f
mamlaki <melekredwan@icloud.com>
2023-09-07 16:30:47 -0600

    Added temp styles for rendered weather data.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
-import Login from '@/components/Login'
-import Navbar from '@/components/Navbar'
-import TestWeatherFetch from '@/components/TestWeatherFetch'
 import { useWeather } from '@/utils/weathercontext'
+
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
+import Grid from '@mui/material/Grid'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,13 +23,24 @@ export default function Home() {
       <header>
       </header>
       <main>
-        {/* <TestWeatherFetch /> */}
         {weatherData && (
-          <div>
-            <h1>{ weatherData.name }</h1>
-            <h2>{ weatherData.weather[0].description }</h2>
-            <h3>{ Math.round(weatherData.main.temp - 273.15) }ºC</h3>
-          </div>
+          <Grid container spacing={2} justifyContent={'center'}>
+            <Grid item xs={12} sm={6} md={4}>
+              <Card>
+                <CardContent>
+                  <Typography variant='h5' component='div'>
+                    { weatherData.name }
+                  </Typography>
+                  <Typography variant='subtitle1' color='textSecondary'>
+                    { weatherData.weather[0].description }
+                  </Typography>
+                  <Typography variant='h6'>
+                    { Math.round(weatherData.main.temp - 273.15) }ºC
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
         )}
       </main>
       <footer>
