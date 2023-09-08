b1a0e3e04060dea05e2a93ecfcf896329f9e1f21
mamlaki <melekredwan@icloud.com>
2023-09-07 18:17:02 -0600

    Added temp conversion functionality.

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { useState } from 'react'
 import { Inter } from 'next/font/google'
 import { useWeather } from '@/utils/weathercontext'
 
@@ -6,11 +7,34 @@ import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Grid'
+import IconButton from '@mui/material/IconButton'
+import DeviceThermostatIcon from '@mui/icons-material/DeviceThermostat';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const { weatherData } = useWeather()
+  const [tempUnit, setTempUnit] = useState('C')
+
+  const toggleTempUnit = () => {
+    if (tempUnit === 'C') {
+      setTempUnit('F')
+    } else if (tempUnit === 'F') {
+      setTempUnit('K')
+    } else {
+      setTempUnit('C')
+    }
+  }
+
+  const convertTemp = (kelvinTemp) => {
+    if (tempUnit === 'C') {
+      return Math.round(kelvinTemp - 273.15)
+    } else if (tempUnit === 'F') {
+      return Math.round((kelvinTemp - 273.15) * 9 / 5 + 32)
+    } else {
+      return Math.round(kelvinTemp)
+    }
+  }
 
   return (
     <>
@@ -34,9 +58,20 @@ export default function Home() {
                   <Typography variant='subtitle1' color='textSecondary'>
                     { weatherData.weather[0].description }
                   </Typography>
-                  <Typography variant='h6'>
-                    { Math.round(weatherData.main.temp - 273.15) }ºC
-                  </Typography>
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <Typography
+                      variant='h6'
+                      component='span'
+                      onClick={toggleTempUnit}
+                      style={{cursor: 'pointer'}}
+
+                    >
+                      {convertTemp(weatherData.main.temp)}º{tempUnit}
+                    </Typography>
+                    <IconButton size='small' onClick={toggleTempUnit}>
+                      <DeviceThermostatIcon />
+                    </IconButton>
+                  </div>
                 </CardContent>
               </Card>
             </Grid>
