4454ad47172f03e4bbba5c1563d42194f0b2ef94
mamlaki <melekredwan@icloud.com>
2023-09-09 14:21:45 -0600

    Cleared up index.js a bit and added some temp layout changes.

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,11 @@
 import Head from 'next/head'
-import { useState } from 'react'
 import { Inter } from 'next/font/google'
 import { useWeather } from '@/utils/WeatherContext'
 
-import Card from '@mui/material/Card'
-import CardContent from '@mui/material/CardContent'
+
 import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Grid'
-import IconButton from '@mui/material/IconButton'
-import DeviceThermostatIcon from '@mui/icons-material/DeviceThermostat';
-import DeleteIcon from '@mui/icons-material/Delete'
+import Container from '@mui/material/Container'
 
 import WeatherCard from '@/components/WeatherCard'
 
@@ -22,8 +18,6 @@ export default function Home() {
     setWeatherData(prevWeatherData => {
       return prevWeatherData.filter((data, i) => i !== index)
     })
-    // const updatedWeatherData = weatherData.filter(data => data.name !== cityName)
-    // setWeatherData(updatedWeatherData)
   }
 
   return (
@@ -48,7 +42,23 @@ export default function Home() {
               )
             })
           ) : (
-            <Typography variant='h6'>No cities planned yet.</Typography>
+            <Container style={{ minHeight: '100vh', backgroundColor: '#F5F5F5' }}>
+              <Grid
+                container
+                spacing={0}
+                direction='column'
+                alignItems='center'
+                justifyContent='center'
+                style={{ minHeight: '100vh' }}
+              >
+                <Grid item xs={12}>
+                  <Typography variant='h6'>No cities added yet</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant='h6'>😔</Typography>
+                </Grid>
+              </Grid>
+            </Container>
           )
         }
       </main>
