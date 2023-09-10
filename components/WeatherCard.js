3a447b5d4b8b012b4bd831d29d725678fce560d3
mamlaki <melekredwan@icloud.com>
2023-09-10 15:09:04 -0600

    Added weather icons to cards based on conditions.

--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -7,7 +7,13 @@ import CardActions from '@mui/material/CardActions'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import IconButton from '@mui/material/IconButton'
+
+
 import DeviceThermostatIcon from '@mui/icons-material/DeviceThermostat'
+import WbSunnyIcon from '@mui/icons-material/WbSunny'
+import CloudIcon from '@mui/icons-material/Cloud'
+import FlashOnIcon from '@mui/icons-material/FlashOn'
+import AcUnitIcon from '@mui/icons-material/AcUnit'
 
 export default function WeatherCard({ data, onDelete }) {
   const [tempUnit, setTempUnit] = useState('C')
@@ -36,15 +42,32 @@ export default function WeatherCard({ data, onDelete }) {
     return countryCode.toUpperCase().replace(/./g, char => String.fromCodePoint(char.charCodeAt(0) + 127397))
   }
 
+  const getWeathericon = (weatherId) => {
+    if (weatherId >= 200 && weatherId <= 232) {
+      return <FlashOnIcon style={{ color: 'yellow' }} />
+    } else if (weatherId >= 300 && weatherId <= 321) {
+      return <AcUnitIcon style={{ color: 'blue' }} />
+    } else if (weatherId >= 500 && weatherId <= 531) {
+      return <CloudIcon style={{ color: 'gray' }} />
+    } else if (weatherId >= 800 && weatherId <= 801) {
+      return <WbSunnyIcon style={{ color: 'orange' }} />
+    } else {
+      return <CloudIcon style={{ color: 'gray' }} />
+    }
+  }
+
   return (
     <Card style={{ maxWidth: '300px', width: '100%' }}>
       <CardContent>
         <Typography variant='h5' component='div'>
           { data.name } { countryCodeToFlag(data.sys.country) }
         </Typography>
-        <Typography variant='subtitle1' color='textSecondary'>
-          { data.weather[0].description }
-        </Typography>
+        <div style={{ display: 'flex', alignItems: 'center', gap: '0.4rem' }}>
+          { getWeathericon(data.weather[0].id) }
+          <Typography variant='subtitle1' color='textSecondary'>
+            { data.weather[0].description }
+          </Typography>
+        </div>
         <div style={{ display: 'flex', alignItems: 'center' }}>
           <Typography 
             variant='h6'
