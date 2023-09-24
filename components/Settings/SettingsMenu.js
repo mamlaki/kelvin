d35df63fa2ac128005149283ba1f2586b8e798d1
mamlaki <melekredwan@icloud.com>
2023-09-23 22:08:43 -0600

    SettingsMenu.js: Improved UI and UX. - Replaced default html alerts with MUI Alert and Snackbar components. - Reworked the 'SAVE & EXIT' button to be just a save button.

--- components/Settings/SettingsMenu.js
+++ components/Settings/SettingsMenu.js
@@ -11,12 +11,14 @@ import { useTempUnit } from '@/utils/contexts/TempUnitContext';
 import { useThemeMode } from '@/utils/contexts/ThemeContext';
 
 // MUI Components
+import Alert from '@mui/material/Alert'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
+import Snackbar from '@mui/material/Snackbar'
 
 // Local Components / Other
 import ColorThemeSelector from './ColorThemeSelector';
@@ -30,12 +32,15 @@ export default function SettingsMenu({ settingsOpen, handleSettingsToggle, color
   const { defaultTempUnit, setDefaultTempUnit } = useTempUnit()
   const { darkMode, toggleDarkMode } = useThemeMode()
 
+  const settingsRef = useRef(null);
+
   const [recentColors, setRecentColors] = useState([])
   const [hasUnsavedChanges, setHasUnsavedChanges] = useState(false)
   const [unsavedChangesDialogOpen, setUnsavedChangesDialogOpen] = useState(false)
   const [changes, setChanges] = useState([])
-
-  const settingsRef = useRef(null);
+  const [openSnackbar, setOpenSnackbar] = useState(false)
+  const [alertMessage, setAlertMessage] = useState('')
+  const [alertSeverity, setAlertSeverity] = useState('success')
 
   const DEFAULTS = {
     TEMP_UNIT: 'C',
@@ -43,12 +48,19 @@ export default function SettingsMenu({ settingsOpen, handleSettingsToggle, color
     COLOR_THEME: '#4994EC',
     RECENT_COLORS: []
   }
+
   const [originalSettings, setOriginalSettings] = useState({
     defaultTempUnit: null,
     darkMode: null,
     colorTheme: null,
     recentColors: null
   })
+
+  const showAlertHelper = (message, severity = 'success') => {
+    setAlertMessage(message)
+    setAlertSeverity(severity)
+    setOpenSnackbar(true)
+  }
 
   const checkUnsavedChanges = () => {
     const changesExist = defaultTempUnit !== originalSettings.defaultTempUnit ||
@@ -71,8 +83,7 @@ export default function SettingsMenu({ settingsOpen, handleSettingsToggle, color
 
     if (!session) {
       saveSettingsToLocalStorage(settingsData)
-      setOriginalSettings(settingsData)
-      alert('Settings saved locally. login to save settings across devices.')
+      showAlertHelper('Settings saved locally. Log in to save settings across devices.')
       return
     }
 
@@ -84,8 +95,9 @@ export default function SettingsMenu({ settingsOpen, handleSettingsToggle, color
       
       setOriginalSettings(settingsData)
       setHasUnsavedChanges(false)
-      alert('Settings saved successfully!')
+      showAlertHelper('Settings saved successfully.')
     } catch (error) {
+      showAlertHelper('Failed to save settings.', 'error')
       console.error('Failed to save settings: ', error)
     }
   }
@@ -161,7 +173,6 @@ export default function SettingsMenu({ settingsOpen, handleSettingsToggle, color
   const handleSaveAndExit = () => {
     saveSettingsToBackend()
     setHasUnsavedChanges(false)
-    handleSettingsToggle()
   }
   
   const deleteUser = async () => {
@@ -169,13 +180,13 @@ export default function SettingsMenu({ settingsOpen, handleSettingsToggle, color
       const isSuccess = await deleteUserFromAPI(session.user.id)
 
       if (isSuccess) {
-        alert('Your account was successfully deleted.')
+        showAlertHelper('Your account was successfully deleted.')
         handleSettingsToggle()
         await signOut({ redirect: true, callbackUrl: '/' })
       }
     } catch (error) {
+      showAlertHelper('An error occurred. Please try again later.', 'error')
       console.error('Error deleting user account: ', error)
-      alert('An error occurred. Please try again later.')
     }
   }
 
@@ -186,8 +197,33 @@ export default function SettingsMenu({ settingsOpen, handleSettingsToggle, color
     }
   }
 
+  const messageLines = alertMessage.split('.').filter(line => line.trim() !== '')
+
   return (
     <>
+        <Snackbar
+          open={openSnackbar}
+          autoHideDuration={3000}
+          onClose={() => setOpenSnackbar(false)}
+          anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        >
+          <Alert
+            severity={alertSeverity}
+            onClose={() => setOpenSnackbar(false)}
+            sx={{ 
+              display: 'flex',
+              alignItems: 'center',
+              textAlign: 'center', 
+            }}
+          >
+            {messageLines.map((line, index) => (
+              <span key={index}>
+                {line.trim()}.
+                {index !== messageLines.length - 1 && <br />}
+              </span>
+            ))}
+          </Alert>
+        </Snackbar>
       <Dialog
         open={settingsOpen}
         onClose={() => {
@@ -205,7 +241,7 @@ export default function SettingsMenu({ settingsOpen, handleSettingsToggle, color
           sx: { width: '80%'}
         }}
       >
-      <DialogTitle id='settings-dialog-title'>Settings</DialogTitle>
+        <DialogTitle id='settings-dialog-title'>Settings</DialogTitle>
         <DialogContent sx={{ minHeight: 400, position: 'relative' }}>
           <TempUnitSelector 
             defaultTempUnit={defaultTempUnit}
@@ -263,7 +299,7 @@ export default function SettingsMenu({ settingsOpen, handleSettingsToggle, color
             setChanges([])
             handleSaveAndExit()
           }} color='success' variant='contained'>
-            Save & Exit
+            Save
           </Button>
         </DialogActions>
       </Dialog>
@@ -284,6 +320,7 @@ export default function SettingsMenu({ settingsOpen, handleSettingsToggle, color
           setChanges([])
           setUnsavedChangesDialogOpen(false)
           handleSaveAndExit()
+          handleSettingsToggle()
         }}
       />
     </>
